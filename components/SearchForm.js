510291c6db5636cfa0b80edd88b04da1131e3c91
pkhodakovsky <pavel.khodakovsky1996@gmail.com>
2023-02-19 11:29:23 +0200

    Added loading state to SearchForm

--- components/SearchForm.js
+++ components/SearchForm.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 const SearchForm = () => {
   const [searchValue, setSearchValue] = useState("");
+  const [isSearching, setIsSearching] = useState(false);
 
   const handleInputChange = (event) => {
     setSearchValue(event.target.value);
@@ -17,19 +18,21 @@ const SearchForm = () => {
       className="flex justify-center items-center h-screen"
       onSubmit={handleSearch}
     >
-      <div className="flex flex-wrap justify-center sm:items-stretch sm:w-full sm:max-w-xl md:max-w-2xl">
+      <div className="flex flex-wrap justify-center sm:items-stretch sm:w-full sm:max-w-xl md:max-w-2xl text-xl leading-tight">
         <input
           type="text"
-          className="px-5 py-4 border placeholder:text-gray-900 text-xl outline-none w-full sm:flex-1 sm:w-auto"
+          className="px-5 py-4 border placeholder:text-gray-900 outline-none w-full sm:flex-1 sm:w-auto disabled:opacity-75 disabled:pointer-events-none"
           defaultValue={searchValue}
           onChange={handleInputChange}
           placeholder="Enter image URL"
+          disabled={isSearching}
         />
         <button
           type="submit"
-          className="mt-3 sm:mt-0 px-8 py-4 bg-custom-blue hover:bg-blue-600 text-xl text-white w-full sm:w-auto"
+          className="mt-3 sm:mt-0 px-8 py-4 bg-custom-blue hover:bg-blue-600 text-white w-full sm:w-auto disabled:opacity-75 disabled:pointer-events-none"
+          disabled={isSearching}
         >
-          Search
+          {isSearching ? <span>Wait...</span> : <span>Search</span>}
         </button>
       </div>
     </form>
