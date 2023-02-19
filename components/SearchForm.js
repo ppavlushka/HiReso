06f9f8bad9329f06831f94f3e171b98f5dbf54bc
pkhodakovsky <pavel.khodakovsky1996@gmail.com>
2023-02-19 10:36:59 +0200

    Added Search form

--- components/SearchForm.js
+++ components/SearchForm.js
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
-function SearchForm() {
-  return <div>SearchForm</div>;
-}
+const SearchForm = () => {
+  const [searchValue, setSearchValue] = useState("");
+
+  const handleInputChange = (event) => {
+    setSearchValue(event.target.value);
+  };
+
+  const handleSearch = (event) => {
+    event.preventDefault();
+    console.log(searchValue); // replace with actual search logic
+  };
+
+  return (
+    <form className="flex justify-center items-center h-screen">
+      <div className="flex items-center">
+        <input
+          type="text"
+          className="px-4 py-2 border outline-none"
+          value={searchValue}
+          onChange={handleInputChange}
+          placeholder="Search..."
+        />
+        <button
+          className="px-4 py-2 bg-blue-500 text-white"
+          onClick={handleSearch}
+        >
+          Search
+        </button>
+      </div>
+    </form>
+  );
+};
 
 export default SearchForm;
