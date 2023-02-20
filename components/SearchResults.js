fe6b90f15ade2f14d402a399b3a7b24d10c89e74
pkhodakovsky <pavel.khodakovsky1996@gmail.com>
2023-02-20 10:49:19 +0200

    Limit number of links to 6

--- components/SearchResults.js
+++ components/SearchResults.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 
-const SearchResults = ({ imageUrl, links, onSelect = () => {} }) => {
+const SearchResults = ({ imageUrl, links, onSelect = () => {}, limit = 6 }) => {
   return (
     <div className="w-full md:max-w-2xl lg:max-w-screen-lg mx-auto">
       <div className="text-lg font-medium mb-3">Preview</div>
@@ -16,7 +16,7 @@ const SearchResults = ({ imageUrl, links, onSelect = () => {} }) => {
       </div>
       <div className="text-lg font-medium mb-3">Download Links:</div>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-3.5 text-lg">
-        {links.map((link, index) => (
+        {links.slice(0, limit).map((link, index) => (
           <button
             key={index}
             className="px-8 py-4 bg-custom-darkgray text-white text-center"
